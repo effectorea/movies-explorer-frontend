c8df540ed9f497c83d1abb3ad7759eda058ae5e1
Romanov Eugene <effectorea@yandex.ru>
2022-06-03 18:28:26 +0300

    added window size adjust

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 import { CurrentUserContext } from '../../context/CurrentUserContext';
 import { Route, Switch } from 'react-router-dom';
 import Header from '../Header/Header';
 import Register from '../Register/Register';
 import Login from '../Login/Login';
+import useWindowWidth from '../../utils/useWindowWidth';
 
 function App() {
   const [currentUser, setCurrentUser] = useState({});
+  const width = useWindowWidth();
 
   return (
     <div className='App'>
       <CurrentUserContext.Provider value={currentUser}>
         <Switch>
           <Route exact path='/'>
-            <Header />
+            <Header width={width} />
           </Route>
           <Route path='/sign-up'>
             <Register />
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,10 +3,11 @@ import './Header.css';
 import headerLogo from "../../images/logo.svg";
 import { Route, Switch, Link } from "react-router-dom";
 
-function Header() {
+function Header({width}) {
   return (
       <header className='header'>
         <img src={headerLogo} alt='Логотип Место' className='header__logo' />
+        <h2>{ width <= '768' ? 'Hello' : 'Fuck off'}</h2>
         <div className="header__twin"></div>
       </header>
   );
