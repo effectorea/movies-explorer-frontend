b877dea0dcefcf6afc714396674a6d91c62cca06
Romanov Eugene <effectorea@yandex.ru>
2022-06-10 16:10:14 +0300

    created search form

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState } from 'react';
 import './Movies.css';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,12 +1,44 @@
-import React from "react";
+import React, { useState } from 'react';
 import './SearchForm.css';
+import search from '../../images/search-icon.svg';
 
-function SearchForm () {
-    return (
-        <section className="searchForm">
+function SearchForm() {
 
-        </section>
-    )
-};
+    const [isChecked, setIsChecked] = useState(false);
+
+    const toggleCheckbox = () => {
+        setIsChecked(!isChecked);
+        console.log(isChecked);
+    }
+  return (
+    <section className='search'>
+    <div className='search__section'>
+    <div className='search__block'>
+        <img src={search} alt='Лупа' className='search__image' />
+        <form className='search__form' noValidate>
+          <fieldset className='search__field'>
+            <input
+              className='search__input'
+              type='text'
+              placeholder='Фильм'
+              required
+            />
+            <button className='search__submit' type='submit' value='' />
+          </fieldset>
+        </form>
+      </div>
+
+      <section className='filter'>
+        <label className={ !isChecked ? 'filter__checkbox filter__checkbox_active' : 'filter__checkbox' }>
+          <input onClick={toggleCheckbox} type='checkbox' className='filter__input' />
+        </label>
+        <p className='filter__subtitle'>Короткометражки</p>
+      </section>
+        
+    </div>
+      
+    </section>
+  );
+}
 
 export default SearchForm;
