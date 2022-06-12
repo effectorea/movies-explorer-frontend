9d1cf21afa4809eaa379312ea0c6fae8c1fdd8ec
Romanov Eugene <effectorea@yandex.ru>
2022-06-12 17:35:01 +0300

    ceated filtercheckbox section

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,42 +1,34 @@
 import React, { useState } from 'react';
 import './SearchForm.css';
 import search from '../../images/search-icon.svg';
+import FilterCheckbox from '../FilterCheckbox/FilterCheckbox';
 
 function SearchForm() {
+  const [isChecked, setIsChecked] = useState(false);
 
-    const [isChecked, setIsChecked] = useState(false);
-
-    const toggleCheckbox = () => {
-        setIsChecked(!isChecked);
-        console.log(isChecked);
-    }
+  const toggleCheckbox = () => {
+    setIsChecked(!isChecked);
+    console.log(isChecked);
+  };
   return (
     <section className='search'>
-    <div className='search__section'>
-    <div className='search__block'>
-        <img src={search} alt='Лупа' className='search__image' />
-        <form className='search__form' noValidate>
-          <fieldset className='search__field'>
-            <input
-              className='search__input'
-              type='text'
-              placeholder='Фильм'
-              required
-            />
-            <button className='search__submit' type='submit' value='' />
-          </fieldset>
-        </form>
+      <div className='search__section'>
+        <div className='search__block'>
+          <img src={search} alt='Лупа' className='search__image' />
+          <form className='search__form' noValidate>
+            <fieldset className='search__field'>
+              <input
+                className='search__input'
+                type='text'
+                placeholder='Фильм'
+                required
+              />
+              <button className='search__submit' type='submit' value='' />
+            </fieldset>
+          </form>
+        </div>
+        <FilterCheckbox isChecked={isChecked} toggleCheckbox={toggleCheckbox} />
       </div>
-
-      <section className='filter'>
-        <label className={ !isChecked ? 'filter__checkbox filter__checkbox_active' : 'filter__checkbox' }>
-          <input onClick={toggleCheckbox} type='checkbox' className='filter__input' />
-        </label>
-        <p className='filter__subtitle'>Короткометражки</p>
-      </section>
-        
-    </div>
-      
     </section>
   );
 }
